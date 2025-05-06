d6dd62b183eb2f8ab5d01c9e5a44e739c31be512
Bohdi Norvell <143441832+1106-Norvell-Bohdi@users.noreply.github.com>
2025-05-05 21:18:18 -0700

    Update final-proj.ino
    - implemented LCD_Update
    - implemented store_event
    - added UART for serial.print functionality without using the arduino serial function

--- final-proj.ino
+++ final-proj.ino
@@ -6,19 +6,58 @@
 
 // Pin Definitions
 #define WATER_SENSOR A0
-#define START 2
+#define START 2 // I think start/stop button should be same button 
 #define STOP 3
 #define RESET 4
-#define YELLOW 5
-#define GREEN 6
-#define RED 7
-#define BLUE 8
+
+#define YELLOW 5 // DISABLE
+#define GREEN 6 // IDLE
+#define RED 7  // ERROR
+#define BLUE 8 // RUNNNG
+
 #define FAN 9
 #define MOTOR_1 10
 #define MOTOR_2 11
 #define MOTOR_3 12
 #define MOTOR_4 13
 #define DHT_PIN A1
+
+// UART REGISTER (CANT USE serial.print etc)
+#define RDA 0x80
+#define TBE 0x20
+
+volatile unsigned char* myUCSR0A = (unsigned char*)0x00C0;
+volatile unsigned char* myUCSR0B = (unsigned char*)0x00C1;
+volatile unsigned char* myUCSR0C = (unsigned char*)0x00C2;
+volatile unsigned int*  myUBRR0  = (unsigned int*) 0x00C4;
+volatile unsigned char* myUDR0   = (unsigned char*)0x00C6;
+
+void U0init(unsigned long U0baud){
+    unsigned long FCPU = 16000000;
+    unsigned int tbaud = (FCPU / 16 / U0baud) - 1;
+    *myUCSR0A = 0x20;
+    *myUCSR0B = 0x18;
+    *myUCSR0C = 0x06;
+    *myUBRR0 = tbaud;
+}
+
+void U0putchar(unsigned char U0data){
+    while(!(*myUCSR0A & TBE));
+    *myUDR0 = U0data;
+}
+
+void UART_print(const char* msg){
+  DateTime now = rtc.now();
+  U0putchar(now.timestamp());
+  while(*msg != '\0'){
+    U0putchar(*msg++);
+  }
+}
+
+void UART_println(const char* msg){
+  UART_print(msg);
+  U0putchar('\n');
+}
 
 // Function Prototypes
 void start_button();
@@ -43,7 +82,7 @@ bool systemEnabled = false;
 unsigned long lastUpdateTime = 0;
 
 void setup(){
-  
+  U0init(9600);
 }
 
 void loop(){
@@ -90,12 +129,14 @@ void state_trans(CoolerState newState){
 void start_button(){
     if (currentState == DISABLED){
         state_trans(IDLE);
+        store_event("Start System");
     }
 }
 
 void stop_button(){
     if (currentState != DISABLED){
         state_trans(DISABLED);
+        store_event("Stopped System");
     }
 }
 
@@ -104,6 +145,37 @@ void reset_button(){
         int waterLevel = analogRead(WATER_SENSOR);
         if (waterLevel > WATER_LEVEL_THRESHOLD){
             state_trans(IDLE);
+          store_event("System Reset");
         }
     }
+}
+
+void update_LCD(float temp, float humidity){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" Temp(°C): ");
+  lcd.print(temp);
+  lcd.print(" Humidity(%): ");
+  lcd.print(humidity);
+
+  lcd.setCursor(0,1);
+  lcd.print("State: ");
+  switch(currentState){
+    case DISABLED: 
+      lcd.print("DISABLED");
+      break;
+    case IDLE: 
+      lcd.print("IDLE");
+      break;
+    case RUNNING:
+      lcd.print("RUNNING");
+      break;
+    case ERROR: 
+      lcd.print("ERROR");
+      break;
+  }
+}
+
+voide store_event(const char* event){
+  UART_println(event);
 }
